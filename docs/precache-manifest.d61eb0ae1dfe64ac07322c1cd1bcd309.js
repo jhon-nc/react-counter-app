c29723aca7c9c850e2930143db46789700b3c0c2
Jhon <alex.554.nc@gmail.com>
2021-04-12 19:13:29 -0500

    Docs agregado

---
+++ docs/precache-manifest.d61eb0ae1dfe64ac07322c1cd1bcd309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d75e79e0463c1116f62c3e0753cf81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f729b2c8f04e3101795",
+    "url": "/static/css/main.5ce4e74c.chunk.css"
+  },
+  {
+    "revision": "c4964ffed4d398a0af80",
+    "url": "/static/js/2.b9176544.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.b9176544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f729b2c8f04e3101795",
+    "url": "/static/js/main.a096c2d7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
